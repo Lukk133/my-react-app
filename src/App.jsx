492cfdf01b8a8b7fccdab17387ac5cbcc46e2dd1
Lukk133 <zielinskil@outlook.com>
2024-02-29 15:54:08 +0100

    Color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import MyComponent from './OnChange.jsx'
+import ColorPicker from './OnChange2.jsx'
 
 function App() {
     return(
         <>
-        <MyComponent/>
+        <ColorPicker/>
 
         </>
     )
